5d00a5bf75a9f04a0d6049fc5d3cc4d0eab5c7fe
Bao Quoc <>
2022-11-16 09:57:22 +0700

    merge master

--- src/pages/welcome.page.js
+++ src/pages/welcome.page.js
@@ -1,77 +1,86 @@
-const { WebDriverAgent } = require('appium-webdriveragent')
-const ActionHelper = require('../helpers/actionHelpers')
-const BasePage = require('./base.page')
-const userData = require('../testData/user.json')
-require('chai').should()
+const { WebDriverAgent } = require("appium-webdriveragent");
+const ActionHelper = require("../helpers/actionHelpers");
+const BasePage = require("./base.page");
+const userData = require("../testData/user.json");
+require("chai").should();
 const uniqueNumber = ActionHelper.randomNumber(5);
 
 class WelcomePage extends BasePage {
   getObjectLocator() {
-    const platform = browser.capabilities.platformName.toLowerCase()
-    return require(`./../screens/native/${platform}/welcome.screen.js`)
+    const platform = browser.capabilities.platformName.toLowerCase();
+    return require(`./../screens/native/${platform}/welcome.screen.js`);
   }
 
   async launchApp() {
-    await ActionHelper.launchApp()
-    await ActionHelper.switchToNativeContext()
+    await ActionHelper.launchApp();
+    await ActionHelper.switchToNativeContext();
   }
 
   launchAppiOS() {
-    ActionHelper.launchApp()
-    ActionHelper.switchToNativeContext()
+    ActionHelper.launchApp();
+    ActionHelper.switchToNativeContext();
   }
 
   async loginWithEmail() {
-    await this.enterText(userData.valid.email)
-    await this.clickButtonOnScreen('CONTINUE')
-    await this.enterText(userData.valid.password)
-    await this.clickButtonOnScreen('SIGN IN')
+    await this.enterText(userData.valid.email);
+    await this.clickButtonOnScreen("CONTINUE");
+    await this.enterText(userData.valid.password);
+    await this.clickButtonOnScreen("SIGN IN");
   }
 
   async registerNewUser() {
-    const email = `automation.smthgoodco+${uniqueNumber}@gmail.com`
-    const username = `QuocBao_${uniqueNumber}`
-    await this.enterText(email)
-    await this.clickButtonOnScreen('CONTINUE')
-    await this.enterText('Bao')
-    await this.clickButtonOnScreen('NEXT')
-    await this.enterText(username)
-    await this.clickButtonOnScreen('NEXT')
-    await this.enterValueInTextField('Password', userData.valid.password)
-    await this.enterValueInTextField('Enter password again', userData.valid.password)
-    await this.clickButtonOnScreen('NEXT')
+    const email = `automation.smthgoodco+${uniqueNumber}@gmail.com`;
+    const username = `QuocBao_${uniqueNumber}`;
+    await this.enterText(email);
+    await this.clickButtonOnScreen("CONTINUE");
+    await this.enterText("Bao");
+    await this.clickButtonOnScreen("NEXT");
+    await this.enterText(username);
+    await this.clickButtonOnScreen("NEXT");
+    await this.enterValueInTextField("Password", userData.valid.password);
+    await this.enterValueInTextField(
+      "Enter password again",
+      userData.valid.password
+    );
+    await this.clickButtonOnScreen("NEXT");
   }
 
   async clickHere() {
-    const el = await this.getSelector().textOnScreen.replace('%s', 'click here')
-    await ActionHelper.swipeVertical('up')
-    await ActionHelper.waitForElement(el)
-    await ActionHelper.click(el)
+    const el = await this.getSelector().textOnScreen.replace(
+      "%s",
+      "click here"
+    );
+    await ActionHelper.swipeVertical("up");
+    await ActionHelper.waitForElement(el);
+    await ActionHelper.click(el);
   }
 
   async verifyAcceptCookies() {
-    const el = await this.getObjectLocator().buttonOnScreen.replace('%s', 'ACCEPT COOKIES')
-    await ActionHelper.waitForElement(el)
-      (await ActionHelper.isVisible(el)).should.true;
+    const el = await this.getObjectLocator().buttonOnScreen.replace(
+      "%s",
+      "ACCEPT COOKIES"
+    );
+    await ActionHelper.waitForElement(el)(await ActionHelper.isVisible(el))
+      .should.true;
   }
 
   async enterMultipleValue(field, value) {
-    const el = await this.getObjectLocator().textField.replace('%s', field)
-    await ActionHelper.waitForElement(el)
-    await ActionHelper.sendText(el, ActionHelper.randomNumber(value))
+    const el = await this.getObjectLocator().textField.replace("%s", field);
+    await ActionHelper.waitForElement(el);
+    await ActionHelper.sendText(el, ActionHelper.randomNumber(value));
   }
 
   async enterValueInTextField(field, value) {
-    const el = await this.getObjectLocator().textField.replace('%s', field)
-    await ActionHelper.waitForElement(el)
-    await ActionHelper.sendText(el, value)
+    const el = await this.getObjectLocator().textField.replace("%s", field);
+    await ActionHelper.waitForElement(el);
+    await ActionHelper.sendText(el, value);
   }
 
   async verifyValueInTextField(value) {
-    const el = await this.getObjectLocator().textField.replace('%s', value)
-    await ActionHelper.waitForElement(el)
-      (await ActionHelper.isVisible(el)).should.true;
+    const el = await this.getObjectLocator().textField.replace("%s", value);
+    await ActionHelper.waitForElement(el);
+    (await ActionHelper.isVisible(el)).should.true;
   }
 }
 
-module.exports = WelcomePage
+module.exports = WelcomePage;
